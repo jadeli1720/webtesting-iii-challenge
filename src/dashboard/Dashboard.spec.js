9d6b13719c95620a8bb46fd9128deb49a1453349
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-19 15:03:31 -0600

    Have all the snapshots working for all three components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+describe('<Dashoboard',() => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard />);
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    //Shows controls
+    // it('Dashboard renders <Controls/>', () => {
+    //     const { getByText} = render(<Dashboard />)
+    //     getByText(/controls/i)
+    // })
+});
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+describe('<Display',() => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Display />);
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+});
